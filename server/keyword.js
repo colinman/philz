f2d3478ea1fc6653e186937a7a5c44cfc15cbc9b
malinajiang <malinaj@stanford.edu>
2015-05-31 09:00:17 -0700

    fixed queue

--- server/keyword.js
+++ server/keyword.js
@@ -56,7 +56,10 @@ module.exports = function(app) {
               if (index > 0) var query_time = track_length / lyrics_words.length * index;
 
               // return track_spotify_id, time
-              if (track_spotify_id && track_spotify_id != "") response.json(200, {'track_spotify_id': track_spotify_id, 'query_time': query_time});
+              if (track_spotify_id && track_spotify_id != "") {
+                response.json(200, {'track_spotify_id': track_spotify_id, 'query_time': query_time});
+                word_queue = [];
+              }
               word_queue.shift();
               STATUS = true;
             });
